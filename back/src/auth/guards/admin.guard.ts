4c2469dce79cb8df1c9322f186a8df395515d1a1
ISary-ge <igorek_sary@mail.ru>
2022-05-23 21:10:21 +0300

    fix: nestjs-actorGuard

--- back/src/auth/guards/admin.guard.ts
+++ back/src/auth/guards/admin.guard.ts
@@ -12,7 +12,7 @@ export class OnlyAdminGuard implements CanActivate {
 	constructor(private reflector: Reflector) {}
 	canActivate(context: ExecutionContext): boolean {
 		const request = context.switchToHttp().getRequest<{ user: UserModel }>()
-		const user = request.body.user
+		const user = request.user
 		console.log(request)
 		if (!user.isAdmin) throw new ForbiddenException('You no have rights!')
 
